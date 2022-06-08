eb87fbeb5475b92d77b2f1acb22f00d130c2cbd7
Isaac <isaac.g.diaz10@gmail.com>
2022-06-08 12:48:41 -0500

    upvotes functionality progress

--- routes/songs.js
+++ routes/songs.js
@@ -7,21 +7,49 @@ const db = require('../db/models');
 // const SongUpvote = require('../db/models/SongUpvote');
 
 
-router.get('/', asyncHandler(async(req, res) => {
+router.get('/', asyncHandler(async (req, res) => {
     const users = await db.User.findAll();
     const songUpvotes = await db.SongUpvote.findAll();
     const songs = await db.Song.findAll(
-    {
-        // includes: [songUpvotes]
-    }
+        {
+            // includes: [songUpvotes]
+        }
     );
-    console.log(songs)
     // // const songgenres = await db.SongGenre.findAll();
 
     let count = 0;
 
 
-    res.render('songs', {songs, users, songUpvotes, count});
+    res.render('songs', { songs, users, songUpvotes, count });
 }));
+
+router.post('/upvote/:id', async (req, res) => {
+    if (req.session.auth) {
+        const songId = req.params.id
+        const { userId } = req.session.auth
+
+        const songUpvotes = await db.SongUpvote.findAll({
+            where: {
+                userId,
+                songId
+            }
+        })
+
+
+        if (!songUpvotes.length) {
+            db.SongUpvote.create({
+                userId,
+                songId
+            })
+        } else {
+            await songUpvotes[0].destroy()
+        }
+        res.redirect("/songs")
+
+
+    } else res.redirect("/users/login") // when user is not logged in, this will redirect them to the login page
+
+})
+
 
 module.exports = router;
