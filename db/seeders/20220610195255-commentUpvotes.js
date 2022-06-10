e250c675e110401e097f589530c0278732132f1a
Jonathan <kim.jonathan426@gmail.com>
2022-06-10 16:19:00 -0400

    commentUpvotes data created

--- db/seeders/20220610195255-commentUpvotes.js
+++ db/seeders/20220610195255-commentUpvotes.js
@@ -9,7 +9,27 @@ module.exports = {
       Example:
       */
     return queryInterface.bulkInsert('CommentUpvotes', [
-      { userId: 1, commentId: 1, createdAt: new Date(), updatedAt: new Date() },
+      { userId: 3, commentId: 1, createdAt: new Date(), updatedAt: new Date() },
+      { userId: 5, commentId: 1, createdAt: new Date(), updatedAt: new Date() },
+      { userId: 3, commentId: 2, createdAt: new Date(), updatedAt: new Date() },
+      { userId: 1, commentId: 6, createdAt: new Date(), updatedAt: new Date() },
+      { userId: 2, commentId: 6, createdAt: new Date(), updatedAt: new Date() },
+      { userId: 3, commentId: 6, createdAt: new Date(), updatedAt: new Date() },
+      { userId: 5, commentId: 6, createdAt: new Date(), updatedAt: new Date() },
+      { userId: 7, commentId: 6, createdAt: new Date(), updatedAt: new Date() },
+      { userId: 9, commentId: 6, createdAt: new Date(), updatedAt: new Date() },
+      { userId: 1, commentId: 11, createdAt: new Date(), updatedAt: new Date() },
+      { userId: 2, commentId: 11, createdAt: new Date(), updatedAt: new Date() },
+      { userId: 3, commentId: 11, createdAt: new Date(), updatedAt: new Date() },
+      { userId: 4, commentId: 11, createdAt: new Date(), updatedAt: new Date() },
+      { userId: 5, commentId: 11, createdAt: new Date(), updatedAt: new Date() },
+      { userId: 6, commentId: 11, createdAt: new Date(), updatedAt: new Date() },
+      { userId: 7, commentId: 11, createdAt: new Date(), updatedAt: new Date() },
+      { userId: 5, commentId: 12, createdAt: new Date(), updatedAt: new Date() },
+      { userId: 1, commentId: 13, createdAt: new Date(), updatedAt: new Date() },
+      { userId: 2, commentId: 13, createdAt: new Date(), updatedAt: new Date() },
+      { userId: 3, commentId: 13, createdAt: new Date(), updatedAt: new Date() },
+      { userId: 6, commentId: 13, createdAt: new Date(), updatedAt: new Date() },
     ], {});
   },
 
