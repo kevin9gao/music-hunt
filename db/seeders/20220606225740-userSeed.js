49feab12da651145aced82105003f50c02fe6f0a
Jonathan <kim.jonathan426@gmail.com>
2022-06-10 13:43:31 -0400

    userSeed data added

--- db/seeders/20220606225740-userSeed.js
+++ db/seeders/20220606225740-userSeed.js
@@ -33,6 +33,8 @@ module.exports = {
         email: 'mi@demo.com',
         username: 'Mr. Demo3',
         full_name: 'Demo Demolitioner',
+        biography: 'I am a demolitioner',
+        profilePic: 'https://img.favpng.com/1/6/20/cartoon-person-illustration-png-favpng-mrhesMTZHc3QDPAZXgJZfMx9T.jpg',
         hashedPassword: 'example-pasword',
         createdAt: new Date(),
         updatedAt: new Date()
@@ -41,6 +43,8 @@ module.exports = {
         email: 'dIssac@gmail.com',
         username: 'iDiaz',
         full_name: 'Issac Diaz',
+        biography: 'I am Issac Diaz',
+        profilePic: 'https://cam.mbi.ufl.edu/wordpress/files/2021/11/no-headshot-processed-processed.png',
         hashedPassword: 'example-pasword',
         createdAt: new Date(),
         updatedAt: new Date()
@@ -49,6 +53,8 @@ module.exports = {
         email: 'kgao@outlook.com',
         username: 'kevkev',
         full_name: 'Kevin Gao',
+        biography: 'I am Kevin Gao',
+        profilePic: 'https://cam.mbi.ufl.edu/wordpress/files/2021/11/no-headshot-processed-processed.png',
         hashedPassword: 'example-pasword',
         createdAt: new Date(),
         updatedAt: new Date()
@@ -57,6 +63,8 @@ module.exports = {
         email: 'Wang.Edward@hotmail.com',
         username: 'Edword',
         full_name: 'Edward Wang',
+        biography: 'I am Xiangyou Edward Wang',
+        profilePic: 'https://cam.mbi.ufl.edu/wordpress/files/2021/11/no-headshot-processed-processed.png',
         hashedPassword: 'example-pasword',
         createdAt: new Date(),
         updatedAt: new Date()
@@ -65,14 +73,38 @@ module.exports = {
         email: 'Jokim@yahoo.com',
         username: 'KimJo',
         full_name: 'Jonathan Kim',
+        biography: 'I am Jonathan Kim',
+        profilePic: 'https://cam.mbi.ufl.edu/wordpress/files/2021/11/no-headshot-processed-processed.png',
         hashedPassword: 'example-pasword',
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        email: 'dIssac@gmail.com',
-        username: 'Yousir name',
-        full_name: 'Jonathan Joestar',
+        email: 'Ninecix@gmail.com',
+        username: 'Ninexcix',
+        full_name: 'Job Woo',
+        biography: 'Job Woo, also known as NINEXCIX or referred to as NINE, is a Korean-American human from Maryland. Wanting to give back what music has done for him, he uses sonics as his canvas.',
+        profilePic: 'https://pbs.twimg.com/profile_images/1430570929172717572/zEN_J2j4_400x400.jpg',
+        hashedPassword: 'example-pasword',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        email: 'Tpain@gmail.com',
+        username: 'T-pain',
+        full_name: 'Faheem Rashad Najm',
+        biography: 'Faheem Rasheed Najm, better known by his stage name T-Pain, is an American rapper, singer, songwriter and record producer. Throughout his career as a singer, T-Pain popularized the creative use of the Auto-Tune pitch correction effect, used with extreme parameter settings to create distinctive vocal sounds.',
+        profilePic: 'https://www2.pictures.zimbio.com/gi/T+Pain+8-J65g_Z5Fsm.jpg',
+        hashedPassword: 'example-pasword',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        email: 'Queen@gmail.com',
+        username: 'Queen',
+        full_name: 'Freddie Mercury',
+        biography: 'Freddie Mercury was a British singer and songwriter, who achieved worldwide fame as the lead vocalist of the rock band Queen. Regarded as one of the greatest singers in the history of rock music, he was known for his flamboyant stage persona and four-octave vocal range.',
+        profilePic: 'https://lyricstranslate.com/files/styles/artist/public/freddie-mercury_sacha-baron-cohen.jpg',
         hashedPassword: 'example-pasword',
         createdAt: new Date(),
         updatedAt: new Date()
