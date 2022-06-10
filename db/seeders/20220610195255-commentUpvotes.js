e945c6ad6dc8502a2372ece6a79193404dbbc29a
Jonathan <kim.jonathan426@gmail.com>
2022-06-10 16:15:45 -0400

    commentSeed data created

--- db/seeders/20220610195255-commentUpvotes.js
+++ db/seeders/20220610195255-commentUpvotes.js
@@ -7,11 +7,10 @@ module.exports = {
       Return a promise to correctly handle asynchronicity.
 
       Example:
-      return queryInterface.bulkInsert('People', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
+      */
+    return queryInterface.bulkInsert('CommentUpvotes', [
+      { userId: 1, commentId: 1, createdAt: new Date(), updatedAt: new Date() },
+    ], {});
   },
 
   down: (queryInterface, Sequelize) => {
@@ -20,7 +19,7 @@ module.exports = {
       Return a promise to correctly handle asynchronicity.
 
       Example:
-      return queryInterface.bulkDelete('People', null, {});
-    */
+      */
+      return queryInterface.bulkDelete('CommentUpvotes', null, {});
   }
 };
