e4f1382bcf9d56f7aa6beb1aa256331784c4c0c8
Isaac <isaac.g.diaz10@gmail.com>
2022-06-12 14:28:23 -0500

    fixed bug where you can like without being logged in

--- public/javascripts/songs-upvotes.js
+++ public/javascripts/songs-upvotes.js
@@ -1,40 +1,43 @@
 window.addEventListener("DOMContentLoaded", (event) => {
   const upvoteButtons = document.getElementsByClassName("upvote_button")
+  const loggedInUser = document.getElementById('current-user').value
 
-  for (let i = 0; i < upvoteButtons.length; i++) {
-    const upvoteButton = upvoteButtons[i]
-    const songId = upvoteButton.id
-    let hasBeenClicked = upvoteButton.dataset.click
-    const upvoteNumber = document.getElementById(`N${songId}`)
-    const icon = document.getElementById(`I${songId}`)
+  if (loggedInUser) {
+    for (let i = 0; i < upvoteButtons.length; i++) {
+      const upvoteButton = upvoteButtons[i]
+      const songId = upvoteButton.id
+      let hasBeenClicked = upvoteButton.dataset.click
+      const upvoteNumber = document.getElementById(`N${songId}`)
+      const icon = document.getElementById(`I${songId}`)
 
-    if (hasBeenClicked === "true") {
-      upvoteButton.style.border = "#004bff 2px solid"
-      icon.style.borderBottom = "11px solid #004bff"
-      icon.style.left = "9px"
-    }
-
-    upvoteButton.onclick = async event => {
-      try {
-        const res = await fetch(`/songs/upvote/${songId}`, {
-          method: "POST",
-        })
-      } catch (e) {
-        console.error(e)
-      }
-
-      if (hasBeenClicked === "false") {
-        hasBeenClicked = "true"
-        upvoteNumber.innerHTML = Number(upvoteNumber.innerHTML) + 1;
+      if (hasBeenClicked === "true") {
         upvoteButton.style.border = "#004bff 2px solid"
         icon.style.borderBottom = "11px solid #004bff"
         icon.style.left = "9px"
-      } else if (hasBeenClicked === "true") {
-        hasBeenClicked = "false"
-        upvoteNumber.innerHTML = Number(upvoteNumber.innerHTML) - 1;
-        upvoteButton.style.border = "1px solid #8c8f94"
-        icon.style.borderBottom = "11px solid #8c8f94"
-        icon.style.left = "10px"
+      }
+
+      upvoteButton.onclick = async event => {
+        try {
+          const res = await fetch(`/songs/upvote/${songId}`, {
+            method: "POST",
+          })
+        } catch (e) {
+          console.error(e)
+        }
+
+        if (hasBeenClicked === "false") {
+          hasBeenClicked = "true"
+          upvoteNumber.innerHTML = Number(upvoteNumber.innerHTML) + 1;
+          upvoteButton.style.border = "#004bff 2px solid"
+          icon.style.borderBottom = "11px solid #004bff"
+          icon.style.left = "9px"
+        } else if (hasBeenClicked === "true") {
+          hasBeenClicked = "false"
+          upvoteNumber.innerHTML = Number(upvoteNumber.innerHTML) - 1;
+          upvoteButton.style.border = "1px solid #8c8f94"
+          icon.style.borderBottom = "11px solid #8c8f94"
+          icon.style.left = "10px"
+        }
       }
     }
   }
