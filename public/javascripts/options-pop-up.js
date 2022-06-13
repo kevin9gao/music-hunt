04175a74ab65908078a417114975d5715607c635
Kevin Gao <100536560+kevin9gao@users.noreply.github.com>
2022-06-13 02:01:07 -0700

    Update options-pop-up.js

--- public/javascripts/options-pop-up.js
+++ public/javascripts/options-pop-up.js
@@ -2,6 +2,8 @@ window.addEventListener("load", event => {
   const optionsPopUp = document.getElementById("options-pop-up-background")
   const options = document.getElementById("options-button")
   const upvoteButton = document.getElementsByClassName("upvote-button")
+  const songId = upvoteButton[0].id
+  const name = songName.value
 
   if (options) {
     options.onclick = event => {
